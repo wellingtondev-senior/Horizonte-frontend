bc7ebf55fed8f52c531f4f5dd26552d2f56db373
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-05-10 19:56:16 -0300

    Criando os path colaborador

---
+++ app/colaborador/procurar/page.tsx
@@ -0,0 +1,14 @@
+import Dashboard from "../components/layout/Dashboard";
+
+const DashPage = () => {
+    return (
+        <Dashboard>
+            <article>
+                
+            </article>
+        </Dashboard>
+
+    );
+}
+
+export default DashPage;
