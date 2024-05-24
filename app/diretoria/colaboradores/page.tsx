4af5eb30053da4192841cbb28adc4d83df9f3941
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-05-24 13:21:41 -0300

    update

--- app/diretoria/colaboradores/page.tsx
+++ app/diretoria/colaboradores/page.tsx
@@ -4,9 +4,9 @@ import { FaUsersGear } from "react-icons/fa6";
 const ColaboradoresPage = () => {
     return (
         <Dashboard>
-            <article className="w-full p-10 max-lg:p-4 flex flex-col items-center pt-[100px]">
+            <article className="w-full p-10 max-lg:p-4 flex flex-col items-center ">
 
-                <div className="w-full rounded-lg shadow-lg bg-white p-4">
+                <div className="w-full rounded-lg shadow-lg bg-white p-4 mt-[70px]">
                     <div className="flex items-center justify-start gap-4">
                         <FaUsersGear className="w-10 h-10 fill-gray-400"/>
                         <span className="text-[25px] font-bold text-gray-500">Colaboradores</span>
