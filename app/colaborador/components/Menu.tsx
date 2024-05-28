c8f9751cb811629019f06af61b2274eeb11a04c0
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-05-28 12:36:32 -0300

    update

--- app/colaborador/components/Menu.tsx
+++ app/colaborador/components/Menu.tsx
@@ -23,7 +23,7 @@ const Navbar = ({ open }: NavBarProps) => {
             <Menu.Header open={open} className="w-full h-auto flex items-center justify-center">
                 <Menu.Action icon={HiOutlineMenuAlt3} />
             </Menu.Header>
-            <Menu.Item icon={TiHome} href="/colaborador/inicio" title="Pagina Inicial"/>
+            <Menu.Item icon={TiHome} href="/colaborador" title="Pagina Inicial"/>
             <Menu.Item icon={FaListUl} href="/colaborador/cadastros" title="Cadastro"/>
             <Menu.Item icon={FaUser} href="/colaborador/perfil" title="Perfil"/>
             <Menu.Item icon={IoSettingsOutline} href="/colaborador/analises" title="Analises"/>
