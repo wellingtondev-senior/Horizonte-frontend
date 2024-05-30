1c96849b68adce2cdf00020832c8e166b0adf795
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-05-30 13:19:58 -0300

    update

--- app/colaborador/painel-controle/page.tsx
+++ app/colaborador/painel-controle/page.tsx
@@ -1,7 +1,14 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Dashboard from "../components/layout/Dashboard";
+import { useRouter } from "next/navigation";
 
 const PainelControlePage = () => {
+
+    const router = useRouter();
+    const push = (path:string)=>{
+        router.push(`/colaborador/painel-controle/${path}`)
+    }
     return (
         <Dashboard>
             <article className="w-full p-10 max-lg:p-4 flex flex-col items-center ">
@@ -9,7 +16,7 @@ const PainelControlePage = () => {
                     <span className="text-[25px] text-white font-bold">Painel de Controle</span>
                 </div>
                 <div className="grid grid-cols-2 w-[563px] max-lg:w-full gap-[1px] max-lg:grid-cols-1">
-                   <Button className="h-[50px] bg-[#77716C] rounded-none rounded-tl-[10px] max-lg:rounded-tr-[10px]">Licenciamento Ambiental</Button>
+                   <Button onClick={()=>push("licenciamento-ambiental")} className="h-[50px] bg-[#77716C] rounded-none rounded-tl-[10px] max-lg:rounded-tr-[10px]">Licenciamento Ambiental</Button>
                    <Button className="h-[50px] bg-[#77716C] rounded-none rounded-tr-[10px] max-lg:rounded-none">Gestão Ambiental de Obras</Button>
                    <Button className="h-[50px] bg-[#77716C] rounded-none ">Resíduos Sólidos</Button>
                    <Button className="h-[50px] bg-[#77716C] rounded-none ">Projetos e Estudos Ambientais</Button>
