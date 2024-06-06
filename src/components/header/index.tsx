cd8e0cdb857658803314fd4ee96946cc16106b68
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-06-06 12:39:57 -0300

    update

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,17 +3,18 @@ import useMenuState, { useMenuStateProps } from "@/store/useMenuState";
 import { HiMenuAlt1 } from "react-icons/hi";
 import { Search } from "./Search";
 import { UserNav } from "./UserNav";
+import { PainelComboBox } from "./PainelComboBox";
 
 const HeaderComponent = () => {
     const menuState = useMenuState((state: useMenuStateProps) => state);
 
     return (
-        <header className="fixed z-[1] w-full h-[50px]  bg-white flex items-center justify-between">
-            <div className="w-full h-[50px] flex items-center justify-start">
+        <header className="fixed z-[1] w-full h-[50px]  bg-white flex items-center justify-between border-b-[1px] border-gray-100">
+            <div className="w-full h-[50px] flex items-center justify-start gap-8">
                 <button onClick={() => menuState.setVisible()} className="text-[20px] text-[#804201] w-[60px] h-[50px] bg-transparent p-4"><HiMenuAlt1 /></button>
-                <h2 className="font-bold text-gray-800 ml-2">Painel do Colaborador</h2>
+                <PainelComboBox/>
             </div>
-            <div className="flex items-center justify-end gap-2 pr-4 border-l-[1px] border-gray-100">
+            <div className="flex items-center justify-end gap-2 pr-4 border-l-[1px] border-gray-100 px-10">
                 <Search/>
                 <UserNav/>
             </div>
