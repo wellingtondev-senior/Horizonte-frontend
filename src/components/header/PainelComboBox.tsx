8dac0425c3b4b1604084b01f602b3705341e7db1
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-06-06 22:20:37 -0300

    update

--- src/components/header/PainelComboBox.tsx
+++ src/components/header/PainelComboBox.tsx
@@ -17,8 +17,17 @@ import {
 } from "@/components/ui/popover";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "@/context/auth.context";
+import { useRouter } from "next/navigation";
+import { Role } from "@/enums/role.enum";
 
-const COMBO = {
+interface ICombo {
+  CLIENTE:{ value: string; label: string }[],
+  COLABORADOR:{ value: string; label: string }[],
+  DIRETOR:{ value: string; label: string }[],
+  MASTER:{ value: string; label: string }[],
+}
+
+const COMBO:ICombo = {
   CLIENTE: [{ value: "cliente", label: "Gestor Cliente" }],
   COLABORADOR: [
     { value: "cliente", label: "Gestor Cliente" },
@@ -34,68 +43,94 @@ const COMBO = {
 };
 
 export function PainelComboBox() {
+  const router = useRouter();
   const [open, setOpen] = useState(false);
   const authContext = useContext(AuthContext);
   const userRole = authContext.isUser?.role || null;
+
+  
   const [value, setValue] = useState("");
   const [isOption, setIsOption] = useState<{ value: string; label: string }[]>(
     []
   );
- 
+  
+    function getAllPaths(url: string): string[] {
+      // Cria uma instância da classe URL a partir da string fornecida
+      const parsedUrl = new URL(url);
+  
+      // Obtém o pathname, que contém a parte do caminho da URL
+      const pathname = parsedUrl.pathname;
+  
+      // Divide o pathname em partes, separando pelos "/"
+      const paths = pathname.split('/').filter(part => part.length > 0);
+  
+      return paths;
+    }
 
   useEffect(() => {
+    const url = window.location.href;
+    const path = getAllPaths(url);
+   
     if (userRole) {
-      setValue(userRole.toLocaleLowerCase());
-      console.log(userRole)
-      setIsOption(COMBO[userRole]);
+      if(!!path[0]){
+        const role = path[0].toLocaleUpperCase() as Role;
+        console.log(role)
+         setValue(path[0]);
+         setIsOption(COMBO[userRole]);
+      }else{
+        setValue(userRole.toLocaleLowerCase);
+        setIsOption(COMBO[userRole]);
+      }
+      
     }
   }, [userRole]);
 
   return (
     <div className="max-sm:hidden">
-          <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between "
-        >
-          {value
-            ? isOption.find((option) => option.value === value)?.label
-            : "Selecione o Gestor..."}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command>
-          <CommandInput placeholder="Buscar gestor..." />
-          <CommandList>
-            <CommandEmpty>Nenhum gestor encontrado.</CommandEmpty>
-            <CommandGroup>
-              {isOption.map((option) => (
-                <CommandItem
-                  key={option.value}
-                  value={option.value}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    setOpen(false);
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === option.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {option.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] justify-between "
+          >
+            {value
+              ? isOption.find((option) => option.value === value)?.label
+              : "Selecione o Gestor..."}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder="Buscar gestor..." />
+            <CommandList>
+              <CommandEmpty>Nenhum gestor encontrado.</CommandEmpty>
+              <CommandGroup>
+                {isOption.map((option) => (
+                  <CommandItem
+                    key={option.value}
+                    value={option.value}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? "" : currentValue);
+                      router.push(`/${currentValue}`)
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === option.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {option.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
 
     </div>
   );
