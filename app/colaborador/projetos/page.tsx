e63db4e7ed286ef5c7f31dc631497ba5db907901
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-06-13 15:02:29 -0300

    update

--- app/colaborador/projetos/page.tsx
+++ app/colaborador/projetos/page.tsx
@@ -64,7 +64,7 @@ const StatusPage = () => {
             <article className="w-full  flex flex-col items-start gap-4 border border-gray-200 shadow-sm rounded-lg p-2">
                 <div className="w-full flex items-center justify-between h-[60px] border-b-[1px] border-gray-200 px-2">
                     <div>
-                        <span className="text-[18px] text-gray-700 font-bold">Projetos</span>
+                        <span className="text-[18px] text-gray-700 font-bold">Lista de Projetos</span>
                     </div>
                     <div>
                         <Button variant="outline">Novo Projeto</Button>
