a1e4c547a4bb1d7116bf05acb1821fe36ff58f30
Wellington Dev <120144343+wellingtondev-senior@users.noreply.github.com>
2024-06-20 19:35:38 -0300

    update

--- src/components/TipoDumentoIcon.tsx
+++ src/components/TipoDumentoIcon.tsx
@@ -1,8 +1,8 @@
 import { DocumentosType } from "@/enums/documento.enum";
 import { BsFileEarmarkPdf } from "react-icons/bs";
 import { FaFile } from "react-icons/fa6";
-
-
+import { FaFileCsv } from "react-icons/fa";
+import { BsFileEarmarkImage } from "react-icons/bs";
 
 interface TipoDumentoIconProps {
     tipo:DocumentosType
@@ -11,24 +11,16 @@ interface TipoDumentoIconProps {
 
 
 const TipoDumentoIcon = ({tipo}:TipoDumentoIconProps) => {
-
      switch (tipo) {
         case DocumentosType.PDF:
             return (<BsFileEarmarkPdf className="fill-red-500 w-6 h-6"/>)
-            
-     
+            case DocumentosType.JPEG:
+            return (<BsFileEarmarkImage className="fill-gray-500 w-6 h-6"/>)
+            case DocumentosType.CSV:
+            return (<FaFileCsv className="fill-green-500 w-6 h-6"/>)
         default:
-            return (<FaFile/>)
+            return (<FaFile className="fill-gray-500 w-6 h-6"/>)
      }
-
-
-
-
-    return (
-        <div>
-            Enter
-        </div>
-    );
 }
 
 export default TipoDumentoIcon;
